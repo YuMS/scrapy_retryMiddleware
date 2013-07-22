62561a659657f56295bcdb1cf6ee2ee9ec8d0d04
YuMS <yumaoshu@gmail.com>
2013-07-22 10:20:05 +0800

    works now

---
+++ middleware.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+from scrapy import log
+from scrapy.contrib.downloadermiddleware.retry \
+    import RetryMiddleware
+
+
+class RetryRecordMiddleware(RetryMiddleware):
+
+    def __init__(self):
+        RetryMiddleware.__init__(self)
+
+    def process_exception(self, request, exception, spider):
+        to_return = RetryMiddleware.process_exception(
+            self, request, exception, spider)
+        retries = request.meta.get('retry_times', 0) + 1
+        log.msg('retries time is %s %d' % (retries, retries))
+        log.msg('max_retry_times is %d' % self.max_retry_times)
+        if retries > self.max_retry_times and request.meta.get('status', 0) == 2:
+            key, value = request.meta.get('pair', ('', ''))
+            key = key.strip()
+            value = value.strip()
+            log.msg('recording failed pair %s\t%s' % (key, value))
+            of = open('check/failed.txt', 'a')
+            of.write('%s\t%s\n' % (key, value))
+            of.close()
+        return to_return
